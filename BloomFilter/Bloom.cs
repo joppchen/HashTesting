43c8510fe32d22c34083289ca419443a241227f3
Jon Petter Grandal <jon.petter.grandal@dnvgl.com>
2021-01-11 10:34:12 +0100

    Implement simple example Bloom Filter

---
+++ BloomFilter/Bloom.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace BloomFilter
+{
+    /// <summary>
+    /// Implementing Bloom Filter to see how it works step by step
+    /// Code copied from the excellent introduction by Mike James: https://www.i-programmer.info/programming/theory/2404-the-bloom-filter.html
+    /// </summary>
+    public class Bloom
+    {
+        private const int m = 1000;
+        private const int k = 5;
+        BitArray bloom = new BitArray(m);
+
+        private int[] hashk(string s, int k)
+        {
+            int[] hashes = new int[k];
+            var temp = s.GetHashCode();
+            hashes[0] = Math.Abs(temp);
+            Random R = new Random(hashes[0]);
+            for (int i = 1; i < k; i++)
+            {
+                hashes[i] = R.Next();
+            }
+
+            return hashes;
+        }
+
+        public void AddData(string s)
+        {
+            int[] hashes = hashk(s, k);
+            for (int i = 0; i < k; i++)
+            {
+                var temp = hashes[i] % m;
+                var temp2 = 0;
+                var temp3 = Math.DivRem(hashes[i], m, out temp2);
+                bloom.Set(temp, true);
+            }
+        }
+
+        public Boolean LookUp(string s)
+        {
+            int[] hashes = hashk(s, k);
+            for (int i = 0; i < k; i++)
+            {
+                var temp1 = hashes[i] % m;
+                var temp2 = bloom[temp1];
+                if (bloom[hashes[i] % m] == false) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BloomFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BloomFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bloom bloom = new Bloom();
+            string data = "Hello Bloom Filter";
+            bloom.AddData(data);
+            Console.WriteLine($"Added '{data}'");
+
+            //MessageBox.Show(bloom.LookUp(data).ToString());
+            Console.WriteLine($"Looking up '{data}'");
+            Console.WriteLine(bloom.LookUp(data).ToString());
+
+            var hello = "Hello, World!";
+            Console.WriteLine($"Looking up '{hello}'");
+            Console.WriteLine(bloom.LookUp(hello).ToString());
+        }
+    }
+}
